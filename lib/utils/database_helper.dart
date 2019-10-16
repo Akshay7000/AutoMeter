49562cefb91bb96b25b7eda8c407e9a16379a707
Akshay patel <aksh23484@gmail.com>
2019-10-16 21:50:00 +0530

    jsssjjs

--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -48,7 +48,7 @@ class DatabaseHelper {
   void _createDb(Database db, int newVersion) async {
 
     await db.execute('CREATE TABLE $noteTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, '
-        '$colDescription TEXT, $colPriority INTEGER, $colDate TEXT, $colEmail TEXT)');
+        '$colDescription TEXT, $colPriority INTEGER, $colDate TEXT, $colEmail TEXT');
   }
 
   // Fetch Operation: Get all note objects from database
